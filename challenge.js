b113c71c1e3a07babc9660e416fa2003ed922cb8
IamKashinoki <104068654+IamKashinoki@users.noreply.github.com>
2022-12-16 11:41:52 +0100

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,3 +1,10 @@
 function chalenge_time() {
 classtoString("challenge")
+  class Challenger {
+  constructor(name, species, witchcraft) {
+    this.name = name;
+    this.species = species;
+    this.witchcraft = witchcraft;
+  }
+}
 }
